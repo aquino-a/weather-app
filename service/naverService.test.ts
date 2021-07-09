f0cc8feffd2805098be02be1c12db1d299618512
Alex <29205589+aquino-a@users.noreply.github.com>
2021-07-09 16:05:23 +0900

    add weather parse impl and test

--- service/naverService.test.ts
+++ service/naverService.test.ts
@@ -1,7 +1,13 @@
 import { location } from './locationService';
 import { naverService } from './naverService';
+import { weather } from './weatherService';
+
+// const jsdom = require("jsdom");
+const { JSDOM } = require("jsdom");
+const fs = require('fs');
 const fetch = require('node-fetch');
 global.fetch = fetch
+
 
 const ns = new naverService();
 
@@ -26,5 +32,19 @@ test('search 봉천동', async () => {
                 { name: '서울특별시 용산구 이태원2동', code: '09170660' },
                 { name: '서울특별시 용산구 이태원1동', code: '09170650' },
             ]);
+
+  });
+
+  test('parse weather', async () => {
+    const rawHtml = fs.readFileSync('./service/weather-parse-test.html')
+    const weatherDoc = new JSDOM(rawHtml).window.document;
+
+    const parsedWeather = naverService.parseWeather(weatherDoc);
+
+    fail();
+    expect(
+        parsedWeather)
+        .toStrictEqual<any>(
+           undefined);
 
   });
--- service/naverService.ts
+++ service/naverService.ts
@@ -1,4 +1,6 @@
+import { template } from "@babel/core";
 import { location, locationService } from "./locationService";
+import { scale, temperature, weather, weatherService } from "./weatherService";
 
 /**
  * A service that communicates with the naver api.
@@ -7,9 +9,12 @@ import { location, locationService } from "./locationService";
  * @class naverService
  * @implements {locationService}
  */
-export class naverService implements locationService {
+export class naverService implements locationService, weatherService {
 
-    static readonly NAVER_BASE_URL: string = "https://ac.weather.naver.com";
+    static readonly NAVER_BASE_URL: string = "weather.naver.com";
+    static readonly TEMPERATURE_REGEX: RegExp = new RegExp('(\\d+)°');
+    static readonly PERCENT_REGEX: RegExp = new RegExp('(\\d+)%');
+    static readonly SPEED_REGEX: RegExp = new RegExp('(\\d+) *m/s');
 
     /**
      * Searchs the naver api for the location name and location code using a text query.
@@ -19,8 +24,8 @@ export class naverService implements locationService {
      * @memberof naverService
      */
     async searchLocation(query: string): Promise<location[]> {
-        
-        const url = new URL(`${naverService.NAVER_BASE_URL}/ac`);
+
+        const url = new URL(`https://ac.${naverService.NAVER_BASE_URL}/ac`);
         url.searchParams.append('q_enc', 'utf-8');
         url.searchParams.append('r_format', 'json');
         url.searchParams.append('r_enc', 'utf-8');
@@ -30,7 +35,7 @@ export class naverService implements locationService {
 
         const response = await fetch(url);
         const result = await response.json();
-        
+
         return this.parseLocations(result);
     }
 
@@ -43,6 +48,76 @@ export class naverService implements locationService {
     parseLocations = (rawObject: any): location[] => {
         return rawObject.items[0]
             .map((a: string[][]) => ({ name: a[0][0], code: a[1][0] }));
-    } 
+    }
+
+    /**
+     * Searchs for the weather based on the location code.
+     *
+     * @param {string} locationCode the code of the location to search for.
+     * @return {*}  {Promise<weather>}
+     * @memberof naverService
+     */
+    async searchWeather(locationCode: string): Promise<weather> {
+
+        const url = new URL(`https://${naverService.NAVER_BASE_URL}/today/${locationCode}`);
+
+        const response = await fetch(url);
+        const rawHtml = await response.text();
+        const domParser = new DOMParser();
+
+        const weatherDoc = domParser.parseFromString(rawHtml, 'text/html');
+
+        return naverService.parseWeather(weatherDoc);
+    }
+
+    /**
+     * Parse the weather from a naver weather page.
+     *
+     * @static
+     * @param {Document} weatherDoc the naver weather web page.
+     * @return {*}  {(weather)} the weather data from the web page.
+     * @memberof naverService
+     */
+    static parseWeather(weatherDoc: Document): weather {
+
+        // weather_area
+        const weatherArea = weatherDoc.getElementsByClassName('weather_area')[0] as HTMLElement;
+        const currentTemperature = naverService.parseTemperature(weatherArea);
+        const listDetails = naverService.parseListDetails(weatherArea);
+        const currentCondition = weatherArea.getElementsByClassName('weather')[0].textContent as string;
+
+        return {
+            temperature: currentTemperature,
+            humidity: listDetails.humidity,
+            windSpeed: listDetails.windSpeed,
+            windDirection: listDetails.windDirection,
+            feel: listDetails.feel,
+            dust: '',
+            microDust: '',
+            condition: currentCondition,
+            rainAmount: 0,
+            rainForecasts: [],
+            humidityForecasts: [],
+            windForecasts: [],
+            temperatureForecasts: []
+        };
+    }
+    
+    private static parseTemperature(currentWeather: Element): temperature {
+        const line = currentWeather.getElementsByClassName('current')[0].textContent;
+        return { degrees: Number(naverService.TEMPERATURE_REGEX.exec(line)[1]), type: scale.C } ;
+    }
+
+    private static parseListDetails(currentWeather: Element): any {
+        const list = currentWeather.getElementsByClassName('summary_list')[0];
+
+        return {
+            humidity: Number(naverService.PERCENT_REGEX.exec(list.children[1].textContent)[1]),
+            windDirection: list.children[2].textContent,
+            windSpeed: Number(naverService.SPEED_REGEX.exec(list.children[3].textContent)[1]),
+            feel: { degrees: Number(naverService.TEMPERATURE_REGEX.exec(list.children[5].textContent)[1]), type: scale.C }
+        };
+    }
 }
+
 
