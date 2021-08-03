819c9e9dfe4668c190eb33f0e0689b834ab83a06
Alex <29205589+aquino-a@users.noreply.github.com>
2021-08-03 16:56:27 +0900

    add style to temperature

--- components/Weather/Current.tsx
+++ components/Weather/Current.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import { weather } from '../../service/weatherService';
 
@@ -16,9 +16,13 @@ const Current = (props: { weather: weather }) => {
     const { weather } = props;
 
     return (
-        <View>
-            <Text>{weather!.temperature.degrees}°</Text>
-            <Text>{weather!.condition}</Text>
+        <View style={{flex: 1}}>
+            <View style={styles.temperature}>
+                <Text style={styles.temperatureText}>
+                    {weather!.temperature.degrees}°
+                </Text>
+            </View>
+            {/* <Text>{weather!.condition}</Text>
             <View>
                 <Text>Humidity: {weather!.humidity}%</Text>
                 <Text>{weather!.windDirection} {weather!.windSpeed}m/s</Text>
@@ -27,11 +31,23 @@ const Current = (props: { weather: weather }) => {
             <View>
                 <Text>Dust: {weather!.dust}</Text>
                 <Text>Micro Dust: {weather!.microDust}</Text>
-            </View>
+            </View> */}
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    temperature: {
+        flex: 3,
+        alignSelf: 'center'
+    },
+
+    temperatureText:{
+        fontSize: 60,
+        padding: 10,
+        fontWeight: 'bold'
+    }
+})
 
 
 export default Current;
