d69564d8fac778f076454ace4bf5c50eebd8677f
Alex <29205589+aquino-a@users.noreply.github.com>
2021-08-06 16:01:28 +0900

    format app.tsx

--- App.tsx
+++ App.tsx
@@ -1,41 +1,41 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import Location from './components/Location';
 import Weather from './components/Weather/Weather';
 
 import { location } from './service/locationService';
 
-
 /**
  * A weather app that uses data from Naver.
  *
  * @export
- * @return {*} 
+ * @return {*}
  */
 export default function App() {
+    const [currentLocation, setCurrentLocation] = useState<location | null>(
+        null,
+    );
 
-  const [currentLocation, setCurrentLocation] = useState<location | null>(null);
+    const onLocationChange = (location: location) => {
+        console.log(`on location change: ${JSON.stringify(location)}`);
+        setCurrentLocation(location);
+    };
 
-  const onLocationChange = (location: location) => {
-    console.log(`on location change: ${JSON.stringify(location)}`);
-    setCurrentLocation(location);
-  }
-
-  return (
-    <View style={styles.container}>
-      <Location onLocationChange={onLocationChange} />
-      <Weather location={currentLocation} />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Location onLocationChange={onLocationChange} />
+            <Weather location={currentLocation} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 30,
-    padding: 10,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-  },
+    container: {
+        flex: 1,
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+    },
 });
