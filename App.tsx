0070ea8df644d5f873e219513e74880995942237
Alex <29205589+aquino-a@users.noreply.github.com>
2021-08-06 09:25:35 +0900

    eject expo

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Location from './components/Location';
@@ -27,7 +26,6 @@ export default function App() {
     <View style={styles.container}>
       <Location onLocationChange={onLocationChange} />
       <Weather location={currentLocation} />
-      <StatusBar style="auto" />
     </View>
   );
 }
